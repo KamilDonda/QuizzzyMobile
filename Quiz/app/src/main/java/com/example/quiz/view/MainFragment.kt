a53f15714fcfec68e7e1de08b269b17a859346ff
Kamil Donda <dondakamil@gmail.com>
2020-12-15 21:10:09 +0100

    Add buttons onClickListener

--- Quiz/app/src/main/java/com/example/quiz/view/MainFragment.kt
+++ Quiz/app/src/main/java/com/example/quiz/view/MainFragment.kt
@@ -6,29 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.quiz.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_start.setOnClickListener {  }
+
+        button_history.setOnClickListener {  }
+
+        button_about.setOnClickListener {  }
     }
 }
