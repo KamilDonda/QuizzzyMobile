e88087e0bfe65b5fa0a25899d9c6a53bfc2dfb96
Kamil Donda <dondakamil@gmail.com>
2020-12-15 18:26:41 +0100

    Create navigation file and Main Fragment

--- Quiz/app/src/main/java/com/example/quiz/view/MainActivity.kt
+++ Quiz/app/src/main/java/com/example/quiz/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.quiz
+package com.example.quiz.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quiz.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
