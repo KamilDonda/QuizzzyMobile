8ff03f19898c30091df02ef83145e28d551afcdf
Kamil Donda <dondakamil@gmail.com>
2020-12-16 10:52:30 +0100

    Add navigation MainFragment -> CategoryFragment

--- Quiz/app/src/main/java/com/example/quiz/view/MainFragment.kt
+++ Quiz/app/src/main/java/com/example/quiz/view/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.quiz.R
 import kotlinx.android.synthetic.main.fragment_main.*
 
@@ -19,7 +20,7 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        button_start.setOnClickListener {  }
+        button_start.setOnClickListener { it.findNavController().navigate(R.id.action_mainFragment_to_categoryFragment) }
 
         button_history.setOnClickListener {  }
 
