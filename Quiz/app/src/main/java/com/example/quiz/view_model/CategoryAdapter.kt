e19f411d297423b7dc9cdd3d7bc3d9dd7f8e6443
Kamil Donda <dondakamil@gmail.com>
2020-12-16 11:33:55 +0100

    Create CategoryAdapter and connect it with recyclerView

---
+++ Quiz/app/src/main/java/com/example/quiz/view_model/CategoryAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.quiz.view_model
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.example.quiz.R
+
+class CategoryAdapter(val categories: List<String>)
+    : RecyclerView.Adapter<CategoryAdapter.Holder>() {
+
+    inner class Holder(view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.category_row, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int = categories.size
+
+    override fun onBindViewHolder(holder: CategoryAdapter.Holder, position: Int) {
+        val button = holder.itemView.findViewById<Button>(R.id.button_category)
+
+        button.text = categories.get(position)
+    }
+
+}
