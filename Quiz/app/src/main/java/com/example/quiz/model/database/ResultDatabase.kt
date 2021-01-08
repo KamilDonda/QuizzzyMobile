7d402b27bd65967219e1e57e549a44853ae04168
Kamil Donda <dondakamil@gmail.com>
2021-01-08 19:57:23 +0100

    Add Converter

--- Quiz/app/src/main/java/com/example/quiz/model/database/ResultDatabase.kt
+++ Quiz/app/src/main/java/com/example/quiz/model/database/ResultDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.quiz.model.converters.Converters
 import com.example.quiz.model.dao.ResultDao
 import com.example.quiz.model.entities.Result
 
@@ -12,6 +14,7 @@ import com.example.quiz.model.entities.Result
     version = 1,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class ResultDatabase: RoomDatabase() {
     abstract fun resultDao(): ResultDao
 
