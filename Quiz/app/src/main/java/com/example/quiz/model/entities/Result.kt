c2b33298ae329890f399e2a63c62bdf7a2b6c9cd
Kamil Donda <dondakamil@gmail.com>
2021-01-08 18:53:13 +0100

    Create Result entity

---
+++ Quiz/app/src/main/java/com/example/quiz/model/entities/Result.kt
@@ -0,0 +1,14 @@
+package com.example.quiz.model.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "result_table")
+data class Result(
+        @PrimaryKey(autoGenerate = true) val id: Int,
+        val date: Date,
+        val category: Int,
+        val difficulty: Int,
+        val result: Int
+)
