dad52f14cfe4bf2874c5e5cc08dacb6ced947f27
Kwolik <kwolikzsp6@gmail.com>
2021-01-08 16:00:34 +0100

    Displaying the result and attaching the view to an arrow

--- Quiz/app/src/main/java/com/example/quiz/view/ResultFragment.kt
+++ Quiz/app/src/main/java/com/example/quiz/view/ResultFragment.kt
@@ -5,13 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.example.quiz.R
+import com.example.quiz.view_model.vm.ResultViewModel
 import kotlinx.android.synthetic.main.fragment_result.*
 
 class ResultFragment : Fragment() {
 
+    private lateinit var viewModelResult: ResultViewModel
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
+
+        viewModelResult = ViewModelProvider(requireActivity()).get(ResultViewModel::class.java)
 
         return inflater.inflate(com.example.quiz.R.layout.fragment_result, container, false)
     }
@@ -19,20 +27,20 @@ class ResultFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val result = 8;
+        val result = viewModelResult.IncrementResultNumber();
 
-        if(result < 4)
+        if (result < 4)
             textViewResultInfo.text = "Maybe next time.."
-        if(result >= 4 && result < 7)
+        if (result >= 4 && result < 7)
             textViewResultInfo.text = "Nice or bad result?"
-        if(result >= 7)
+        if (result >= 7)
             textViewResultInfo.text = "Confratulations!"
 
         textViewResult.text = result.toString() + "/10"
 
         imageViewArrow.setOnClickListener {
-            imageViewArrow.animate().rotation(imageViewArrow.rotation + 360).start()
+            imageViewArrow.animate().rotationBy(imageViewArrow.rotation + 370).withEndAction{ it.findNavController().navigate(R.id.action_resultFragment_to_categoryFragment) }.start()
         }
-    }
 
+    }
 }
