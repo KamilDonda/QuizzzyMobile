8ae362a83563716e2c3bfb3a180d59a239649457
Kwolik <kwolikzsp6@gmail.com>
2021-01-13 16:00:31 +0100

    Add displaying results in recyclerview

--- Quiz/app/src/main/java/com/example/quiz/view/HistoryFragment.kt
+++ Quiz/app/src/main/java/com/example/quiz/view/HistoryFragment.kt
@@ -8,22 +8,40 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.quiz.R
+import com.example.quiz.view_model.adapters.HistoryAdapter
+import com.example.quiz.view_model.vm.HistoryViewModel
 import com.example.quiz.view_model.vm.ResultViewModel
+import kotlinx.android.synthetic.main.fragment_history.*
 
 class HistoryFragment : Fragment() {
 
-    private lateinit var viewModel: ResultViewModel
+    private lateinit var viewModel: HistoryViewModel
+    private lateinit var myadapter: HistoryAdapter
+    private lateinit var mylayoutmanager: LinearLayoutManager
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        viewModel = ViewModelProvider(requireActivity()).get(ResultViewModel::class.java)
+        mylayoutmanager = LinearLayoutManager(context)
+        viewModel = ViewModelProvider(requireActivity()).get(HistoryViewModel::class.java)
+        viewModel.history.observe(viewLifecycleOwner, Observer { myadapter.notifyDataSetChanged() })
+        viewModel.history.value
 
-        viewModel.results.observe(viewLifecycleOwner, Observer {
-            Log.v("TAGGG", it.toString())
-        })
-
+        myadapter = HistoryAdapter(viewModel.history)
         return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = recyclerView_history.apply {
+            layoutManager = mylayoutmanager
+            adapter = myadapter
+        }
+
     }
 }
