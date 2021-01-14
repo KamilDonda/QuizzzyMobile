7302a803edacfc40dd875be563b0ac623c59d93c
Kamil Donda <dondakamil@gmail.com>
2021-01-14 16:11:09 +0100

    Fix landscape history row and add static Category list

--- Quiz/app/src/main/java/com/example/quiz/view/CategoryFragment.kt
+++ Quiz/app/src/main/java/com/example/quiz/view/CategoryFragment.kt
@@ -22,7 +22,7 @@ class CategoryFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
-        val categories = listOfCategories
+        val categories = Category.listOfCategories
 
         viewModel = ViewModelProvider(requireActivity()).get(CategoryViewModel::class.java)
         myAdapter = CategoryAdapter(categories, viewModel)
@@ -37,32 +37,4 @@ class CategoryFragment : Fragment() {
             adapter = myAdapter
         }
     }
-
-    val listOfCategories = listOf(
-            Category(null,"All"),
-            Category(9,"General Knowledge"),
-            Category(10,"Books"),
-            Category(11,"Film"),
-            Category(12,"Music"),
-            Category(13,"Musicals & Theatres"),
-            Category(14,"Television"),
-            Category(15,"Video Games"),
-            Category(16,"Board Games"),
-            Category(17,"Science & Nature"),
-            Category(18,"Computers"),
-            Category(19,"Mathematics"),
-            Category(20,"Mythology"),
-            Category(21,"Sports"),
-            Category(22,"Geography"),
-            Category(23,"History"),
-            Category(24,"Politics"),
-            Category(25,"Art"),
-            Category(26,"Celebrities"),
-            Category(27,"Animals"),
-            Category(28,"Vehicles"),
-            Category(29,"Comics"),
-            Category(30,"Gadgets"),
-            Category(31,"Anime & Manga"),
-            Category(32,"Cartoon & Animations")
-    )
 }
--- Quiz/app/src/main/java/com/example/quiz/view_model/adapters/HistoryAdapter.kt
+++ Quiz/app/src/main/java/com/example/quiz/view_model/adapters/HistoryAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.quiz.view_model.adapters
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quiz.R
+import com.example.quiz.model.Category
 import com.example.quiz.model.entities.Result
 import java.text.SimpleDateFormat
 
@@ -26,9 +28,9 @@ class HistoryAdapter (var history: LiveData<List<Result>>):
         return history.value?.size?:0
     }
 
+    @SuppressLint("SimpleDateFormat", "SetTextI18n")
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: HistoryHolder, position: Int) {
-        //val id = history.value?.get(position).id.toString()
         val date = holder.itemView.findViewById<TextView>(R.id.textView_date)
         val category = holder.itemView.findViewById<TextView>(R.id.textView_category)
         val difficulty = holder.itemView.findViewById<TextView>(R.id.textView_difficultylevel)
@@ -38,8 +40,11 @@ class HistoryAdapter (var history: LiveData<List<Result>>):
         val format = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
         val newDate = format.format(dateFromBDatabase)
 
+        var catId = history.value?.get(position)?.category
+        catId = if(catId == 0) null else catId
+
         date.text = newDate
-        category.text = history.value?.get(position)?.category.toString()
+        category.text = Category.listOfCategories.find { c -> c.id == catId }?.name
         difficulty.text = history.value?.get(position)?.difficulty
         result.text = history.value?.get(position)?.result.toString() + "/10"
     }
