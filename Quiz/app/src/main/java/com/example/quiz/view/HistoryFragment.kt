4950de360b0cbac1ce238ff864e5f52e553c9676
Kamil Donda <dondakamil@gmail.com>
2021-01-22 19:52:16 +0100

    Seach

--- Quiz/app/src/main/java/com/example/quiz/view/HistoryFragment.kt
+++ Quiz/app/src/main/java/com/example/quiz/view/HistoryFragment.kt
@@ -6,15 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quiz.R
+import com.example.quiz.model.Category
 import com.example.quiz.view_model.adapters.HistoryAdapter
 import com.example.quiz.view_model.vm.HistoryViewModel
-import com.example.quiz.view_model.vm.ResultViewModel
 import kotlinx.android.synthetic.main.fragment_history.*
+import kotlin.properties.Delegates
 
 class HistoryFragment : Fragment() {
 
@@ -23,13 +25,14 @@ class HistoryFragment : Fragment() {
     private lateinit var mylayoutmanager: LinearLayoutManager
     private lateinit var recyclerView: RecyclerView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         mylayoutmanager = LinearLayoutManager(context)
         viewModel = ViewModelProvider(requireActivity()).get(HistoryViewModel::class.java)
         viewModel.history.observe(viewLifecycleOwner, Observer { myadapter.notifyDataSetChanged() })
-        viewModel.history.value
 
         myadapter = HistoryAdapter(viewModel.history)
         return inflater.inflate(R.layout.fragment_history, container, false)
@@ -38,10 +41,37 @@ class HistoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        searchView.setQuery(viewModel.query.value, false)
+        searchView.isIconified = false
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                viewModel.history.observe(viewLifecycleOwner, Observer {
+                    if (!query.isNullOrEmpty()) {
+                        viewModel.setQuery(query)
+                        val listOfIds = mutableListOf<Int>()
+                        Category.listOfCategories.forEach {
+                            if (it.name.contains(viewModel.query.value!!))
+                                listOfIds.add(it.id ?: 0)
+                        }
+                        viewModel.setResultsWithFilters(listOfIds)
+                    } else {
+                        viewModel.resetResults()
+                        viewModel.setQuery("")
+                    }
+                    Log.v("TAGGG", "Q: ${viewModel.history.value}")
+                })
+
+                return false
+            }
+        })
+
         recyclerView = recyclerView_history.apply {
             layoutManager = mylayoutmanager
             adapter = myadapter
         }
-
     }
 }
--- Quiz/app/src/main/java/com/example/quiz/view_model/vm/HistoryViewModel.kt
+++ Quiz/app/src/main/java/com/example/quiz/view_model/vm/HistoryViewModel.kt
@@ -1,15 +1,39 @@
 package com.example.quiz.view_model.vm
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.quiz.model.database.ResultDatabase
 import com.example.quiz.model.entities.Result
+import com.example.quiz.model.repository.ResultRepository
 
 class HistoryViewModel (application: Application): AndroidViewModel(application) {
-    val history: LiveData<List<Result>>
+    var history: LiveData<List<Result>>
+    var _history: LiveData<List<Result>>
+
+    private var _query: MutableLiveData<String> = MutableLiveData()
+    val query: LiveData<String>
+        get() = _query
+
+    fun setQuery(text: String){
+        _query.value = text
+    }
+
+    private val repo = ResultRepository(ResultDatabase.getDatabase(application).resultDao())
 
     init{
         history = ResultDatabase.getDatabase(application).resultDao().getAllResults()
+        _history = ResultDatabase.getDatabase(application).resultDao().getAllResults()
+    }
+
+    fun setResultsWithFilters(categories: List<Int>){
+        history = repo.getResultsWithFilters(categories)
+        Log.v("TAGGG", "Q: ${history.value}")
+    }
+
+    fun resetResults() {
+        history = _history
     }
 }
